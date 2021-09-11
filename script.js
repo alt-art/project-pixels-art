f975ebb4c22cb6d87ea90491445e367977bd828d
alt-art <pedromendescraft@gmail.com>
2021-09-11 01:58:11 -0300

    Refactoring generateColor

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 function generateColor() {
-  const rexC = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-  let result = '#';
-  for (let i = 0; i < 6; i += 1) {
-    result += rexC[Math.round(Math.random() * (rexC.length - 1))];
-  }
-  return result;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 const defaultColorPallet = ['black'];
